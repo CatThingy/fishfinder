4ab2bb608baef2a35c7bff707a3abcff449cba9c
CatThingy <andyc.andychen@gmail.com>
2022-09-20 13:26:06 -0700

    fix: actually test + make fishgen_py work

--- fishgen-py/src/lib.rs
+++ fishgen-py/src/lib.rs
@@ -1,22 +1,37 @@
 use ndarray::Array;
 use ndarray::Array3;
+use numpy::IntoPyArray;
 use pyo3::prelude::*;
 
 #[pyclass]
 pub struct PyFishOutput {
     pub image: Array3<f32>,
+    #[pyo3(get)]
     pub fish_x: f32,
+    #[pyo3(get)]
     pub fish_y: f32,
+    #[pyo3(get)]
     pub fish_width: f32,
+    #[pyo3(get)]
     pub fish_height: f32,
+}
+#[pymethods]
+impl PyFishOutput {
+    #[getter]
+    pub fn image<'py, 'b>(&self, m: Python<'py>) -> PyResult<&'b numpy::PyArray3<f32>>
+    where
+        'py: 'b,
+    {
+        Ok(self.image.to_owned().into_pyarray(m))
+    }
 }
 
 impl From<fishgen::FishOutput> for PyFishOutput {
     fn from(fish: fishgen::FishOutput) -> Self {
         let raw_bytes = fish.image.as_raw().iter().map(|v| *v as f32).collect();
         let width = fish.image.width() as usize;
-        let height = fish.image.width() as usize;
-        let new = Array::from_shape_vec((width, height, 3), raw_bytes).unwrap();
+        let height = fish.image.height() as usize;
+        let new = Array::from_shape_vec((width, height, 4), raw_bytes).unwrap();
 
         PyFishOutput {
             image: new.into(),
